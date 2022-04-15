051cb5dfbf84dc656fbd55183411c606a08e4fd4
aust1inn <austinotieno364@gmail.com>
2022-04-15 18:12:00 +0300

    generated new components

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,20 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { GithubAppComponent } from './components/github-app/github-app.component';
+import { GithubProfileComponent } from './components/github-profile/github-profile.component';
+import { GithubProfileCardComponent } from './github-profile-card/github-profile-card.component';
+import { GithubProfileDataComponent } from './components/github-profile-data/github-profile-data.component';
+import { GithubReposComponent } from './components/github-repos/github-repos.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    GithubAppComponent,
+    GithubProfileComponent,
+    GithubProfileCardComponent,
+    GithubProfileDataComponent,
+    GithubReposComponent
   ],
   imports: [
     BrowserModule,
