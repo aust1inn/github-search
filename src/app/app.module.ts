3943f19d85f7afd47fd45979318aa958270bfde6
aust1inn <austinotieno364@gmail.com>
2022-04-15 18:50:42 +0300

    designs the search area

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -5,18 +5,20 @@ import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { GithubAppComponent } from './components/github-app/github-app.component';
 import { GithubProfileComponent } from './components/github-profile/github-profile.component';
-import { GithubProfileCardComponent } from './github-profile-card/github-profile-card.component';
 import { GithubProfileDataComponent } from './components/github-profile-data/github-profile-data.component';
 import { GithubReposComponent } from './components/github-repos/github-repos.component';
+import { NavbarComponent } from './components/navbar/navbar.component';
+import { GithubProfileCardComponent } from './components/github-profile-card/github-profile-card.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     GithubAppComponent,
     GithubProfileComponent,
-    GithubProfileCardComponent,
     GithubProfileDataComponent,
-    GithubReposComponent
+    GithubReposComponent,
+    NavbarComponent,
+    GithubProfileCardComponent
   ],
   imports: [
     BrowserModule,
