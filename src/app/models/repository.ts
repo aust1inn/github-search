07913644ad4a03cb1d64352c3479803355f760b5
aust1inn <austinotieno364@gmail.com>
2022-04-16 09:04:16 +0300

    sets model for the REpository class

--- src/app/models/repository.ts
+++ src/app/models/repository.ts
@@ -1,2 +1,4 @@
 export class Repository {
+    constructor(public name: string, public fullName: string, public description: string, public updatedAt:Date, public htmlUrl: string, public cloneUrl: string, public language: string, public createdAt:Date){}
+
 }
